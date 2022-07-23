ad6377512f66d71ebaa703b33d7bec7a55de4eb8
Jesus Suarez <jesus-s_a@outlook.com>
2022-07-23 01:03:31 -0500

    container mysql && conection db

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 npm start
 npm run start:dev
+
+Ejecutar el container docker mysql
+docker-compose up
+
+## Permisos docker
+
+sudo chmod 777 /var/run/docker.sock
--- src/confg/config.ts
+++ src/confg/config.ts
@@ -1,4 +1,6 @@
 import * as dotenv from 'dotenv';
+import { ConnectionOptions } from 'typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 
 export abstract class ConfigServer {
 	constructor() {
@@ -30,5 +32,22 @@ export abstract class ConfigServer {
 		}
 
 		return '.' + arrayEnv.join('.'); // ['production,', 'env']  => .production.env
+	}
+
+	//el get indica que se debe implementar si o si
+	public get typeORMConfig(): ConnectionOptions {
+		return {
+			type: 'mysql',
+			host: this.getEnviroment('DB_HOST'),
+			port: this.getNumberEnv('DB_PORT'),
+			username: this.getEnviroment('DB_USER'),
+			password: this.getEnviroment('DB_PASSWORD'),
+			database: this.getEnviroment('DB_DATABASE'),
+			entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+			migrations: [__dirname + '/../../migrations/*{.ts,.js}'],
+			synchronize: true,
+			logging: false,
+			namingStrategy: new SnakeNamingStrategy(), //userName => user_name
+		};
 	}
 }
