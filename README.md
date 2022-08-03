97b3641e5668b871e346030eb52714648fdb74fb
Jesus Suarez Alvarez <jesuarez@palace-resorts.local>
2022-08-02 21:14:47 -0500

    clase 4 -P1

--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 npm start
 npm run start:dev
 
-Ejecutar el container docker mysql
+Ejecutar el container docker mysql con el archivo docker-compose.yml
 docker-compose up
 
 ## Permisos docker en linux
-
 sudo chmod 777 /var/run/docker.sock
--- dist/server.js
+++ dist/server.js
@@ -15,7 +15,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
 const cors_1 = __importDefault(require("cors"));
-const user_router_1 = require("./router/user.router");
+const user_router_1 = require("./user/user.router");
 const config_1 = require("./config/config");
 const typeorm_1 = require("typeorm");
 class ServerBootstrap extends config_1.ConfigServer {
